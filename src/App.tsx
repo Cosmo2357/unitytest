291b12b08e15d2bef56141df3cc015afa23de7d2
hayashi.yuhki <hayashi.yuhki@gmail.com>
2022-11-22 19:45:08 +0900

    unity added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Unity, useUnityContext } from "react-unity-webgl";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "Build/uncomplego.loader.js",
+    dataUrl: "Build/uncomplego.data",
+    frameworkUrl: "Build/uncomplego.framework.js",
+    codeUrl: "Build/uncomplego.wasm",
+  });
+  
+  return <Unity 
+  style={{width: "100%", height: "100vh"}}
+  unityProvider={unityProvider} />;
+
 }
 
 export default App;
